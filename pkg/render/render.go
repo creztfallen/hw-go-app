16b06ea7f2524f00b0ebb1450e0e4fa421d5a72a
root <root@mateus-moreno.FLEX.LOCAL>
2022-11-25 14:01:57 -0300

    feat:refactoring the code towork with modules

---
+++ pkg/render/render.go
@@ -0,0 +1,16 @@
+package render
+
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+)
+
+func RenderTemplate(w http.ResponseWriter, tmpl string) {
+	parseTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parseTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error parsing template:", err)
+		return
+	}
+}
